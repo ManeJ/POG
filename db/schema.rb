8ee130def0c6fe8ef1e1bcfa1ad21e47c795359a
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-20 19:53:22 +0200

    form mis en forme pour taille ecran, + implementation logique avatar (modele Avatar, seeds...)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_143025) do
+ActiveRecord::Schema.define(version: 2019_08_20_163208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "avatars", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
