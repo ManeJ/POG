ce70b82ba927534866e1b027353c2837d5f35407
jaures kouassi <jauresko@gmail.com>
2019-08-21 19:03:47 +0200

    card problem solve for index quests

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,18 @@ import { initMapbox } from '../plugins/init_mapbox';
 initAutocomplete();
 initMapbox();
 
-$('button').click(function () {
-  $('button').toggleClass('active');
-  $('.title').toggleClass('active');
-  $('nav').toggleClass('active');
+// $('button').click(function () {
+//   $('button').toggleClass('active');
+//   $('.title').toggleClass('active');
+//   $('nav').toggleClass('active');
+// });
+
+document.querySelectorAll("button").forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.dir(event.currentTarget.parentElement.parentElement.parentElement.parentElement.children[1]);
+    event.currentTarget.classList.toggle("active");
+    event.currentTarget.parentElement.parentElement.parentElement.parentElement.children[1].children[0].classList.toggle("active");
+    event.currentTarget.parentElement.parentElement.parentElement.parentElement.children[1].children[1].classList.toggle("active");
+    event.currentTarget.parentElement.parentElement.parentElement.parentElement.children[1].children[2].classList.toggle("active");
+  });
 });
