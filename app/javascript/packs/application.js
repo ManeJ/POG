d4fabb2b6ccd04267ec7401c2b2325243817d6e0
jaures kouassi <jauresko@gmail.com>
2019-08-22 16:41:29 +0200

    side task like js animation for card and logo with title

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import "../plugins/flatpickr";
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+import '../plugins/details'
 
 initAutocomplete();
 initMapbox();
