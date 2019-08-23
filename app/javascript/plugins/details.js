942ba1bbd98aa64b2a6b53e12d0c7f3b6dbbcdca
Lena EUGENE <lenasurlenet@gmail.com>
2019-08-23 14:08:52 +0200

    js card problem solve

--- app/javascript/plugins/details.js
+++ app/javascript/plugins/details.js
@@ -1,22 +1,27 @@
-const details = document.getElementById('details')
-const x = document.getElementById("more");
-if (x) {
-  x.style.display = 'none'
-  x.style.visibility = 'hidden';
-  x.style.opacity = '0';
-  details.addEventListener("click", () => {
-    if (x.style.display === "none") {
-      window.setTimeout(function () {
-        x.style.display = "block";
-        x.style.visibility = 'visible';
-        x.style.opacity = '1';
-      }, 100)
-    } else {
-      window.setTimeout(function () {
-        x.style.display = "none";
-        x.style.visibility = 'hidden';
-        x.style.opacity = '0';
-      }, 100)
-    }
-  })
-}
+const details = document.querySelectorAll('#details')
+const xs = document.querySelectorAll("#more");
+
+xs.forEach((x)=>{
+  if (x) {
+    x.style.display = 'none'
+    x.style.visibility = 'hidden';
+    x.style.opacity = '0';
+    details.forEach((detail)=>{
+      detail.addEventListener("click", () => {
+        if (x.style.display === "none") {
+          window.setTimeout(function () {
+            x.style.display = "block";
+            x.style.visibility = 'visible';
+            x.style.opacity = '1';
+          }, 100)
+        } else {
+          window.setTimeout(function () {
+            x.style.display = "none";
+            x.style.visibility = 'hidden';
+            x.style.opacity = '0';
+          }, 100)
+        }
+      })
+    })
+  }
+})
