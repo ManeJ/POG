5ac80c8e7599d10bf072d4706b6e7ecc50422650
Lena EUGENE <lenasurlenet@gmail.com>
2019-08-26 19:23:39 +0200

    404. needed to be transposed in the proper file

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,7 @@ Rails.application.routes.draw do
 
   get 'rewards', to:'pages#rewards'
 
+  get 'error_404', to: 'pages#error_404'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
