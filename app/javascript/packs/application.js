cce9f862afc24c1f40cd8bfe3eb31d1fb32d8d3f
Tarik Graoui <tarik.graoui@gmail.com>
2019-08-28 12:43:29 +0200

    fixed.. bad input

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-
 initAutocomplete();
 initMapbox();
 
