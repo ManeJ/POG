0bcf6f1e42407dc11dd76f74ecbc3804ea23119b
Tarik Graoui <tarik.graoui@gmail.com>
2019-08-29 19:11:19 +0200

    clean jaures shit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -64,6 +64,14 @@ $(function(){
   });
 });
 
+function scrollLastMessageIntoView() {
+  const messages = document.querySelectorAll('.message');
+  const lastMessage = messages[messages.length - 1];
+
+  if (lastMessage !== undefined) {
+    lastMessage.scrollIntoView();
+  }
+}
 
 
 
