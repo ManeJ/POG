3d8eff65d5e8498be6d1c9bfaec4a12ff4a5a789
Emma Laurent <emma.laurent.je@gmail.com>
2019-09-13 17:08:01 +0200

    footer desormais responsive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :faq, :rewards]
 
   def home
   end
 
   def profile
   end
-  
+
   def rewards
   end
 
   def faq
   end
-  
+
 end
